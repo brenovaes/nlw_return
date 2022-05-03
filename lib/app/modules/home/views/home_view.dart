aabaf8ebc092ad61be518d957db21edfe9c15fe0
Breno Novaes <brenonovaes@hotmail.com>
2022-05-03 18:05:39 -0300

    ajustando posição do ícone no modalbottomsheet

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -143,7 +143,7 @@ class HomeView extends GetView<HomeController> {
                         ? const SizedBox.shrink()
                         : IconButton(
                             onPressed: () => controller.currentView.value--,
-                            icon: const Icon(Icons.chevron_left),
+                            icon: const Icon(Icons.arrow_back),
                             iconSize: 24,
                             color: const Color(0xFFA1A1AA),
                             padding: EdgeInsets.zero,
@@ -151,17 +151,24 @@ class HomeView extends GetView<HomeController> {
                           ),
                   ),
                 ),
-                Obx(() => Text(
-                      controller.currentView.value == 0
-                          ? 'Deixe seu feedback'
-                          : controller.pagesDescription[
-                                  controller.selectedCard.value as int]['title']
-                              as String,
-                      style: const TextStyle(
-                        color: Color(0xFFF4F4F5),
-                        fontSize: 20,
-                      ),
-                    )),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 24),
+                    child: Center(
+                      child: Obx(() => Text(
+                            controller.currentView.value == 0
+                                ? 'Deixe seu feedback'
+                                : controller.pagesDescription[
+                                        controller.selectedCard.value as int]
+                                    ['title'] as String,
+                            style: const TextStyle(
+                              color: Color(0xFFF4F4F5),
+                              fontSize: 20,
+                            ),
+                          )),
+                    ),
+                  ),
+                ),
               ],
             ),
             const SizedBox(
